c557e00b3232406e89dec535ca97037370cc7495
Ggeroy11 <elnar1995@mail.ru>
2017-09-02 11:45:40 +0600

    Выполнены все задания по дамашке

---
+++ ConsoleApp2/ConsoleApp2/Car.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class Car
+    {
+        private string model;       //реализация 5 закрытых полей
+        private int maxSpeed;
+        private int passengerCapacity;
+        private double price;
+        private int horsePower;
+        private static int wheelAmount; //статические поля
+        private static int helmAmount;
+
+        public string Model     //модель метод доступа к закрытым полям
+        {
+            get
+            {
+                return model;
+            }
+            set
+            {
+                model = value;
+            }
+        }
+
+        public int MaxSpeed         //максимальная скорость метод доступа к закрытым полям
+        {
+            get
+            {
+                return maxSpeed;
+            }
+            set
+            {
+                maxSpeed = value;
+            }
+        }
+
+        public double Price     //цена метод доступа к закрытым полям
+        {
+            get
+            {
+                return price;
+            }
+            set
+            {
+                price = value;
+            }
+        }
+        static Car() {
+            wheelAmount = 4;
+            helmAmount = 1;
+        }
+        public Car()
+        {
+            this.model = null;
+            this.maxSpeed = 0;
+            this.passengerCapacity = 0;
+            this.price = 0;
+            this.horsePower = 0;
+        }
+
+        public Car(string Model, int maxspeed,int passenger,double Price,int horsePower){
+            this.model = Model;
+            this.maxSpeed = maxspeed;
+            this.passengerCapacity = passenger;
+            this.price = Price;
+            this.horsePower = horsePower;
+        }
+
+
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int amountCar=5;
+            Car[] car = new Car[amountCar];
+            for(int i = 0; i < amountCar; i++) {
+                car[i] = new Car();
+            }
+            
+
+        }
+    }
+}
